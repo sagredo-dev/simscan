18b8b3e4ee271ba51e46c1fa3f5be508eb3f2002
sagredo-dev <roberto.puzzanghera@sagredo.eu>
2024-12-22 17:33:49 +0100

    v1.4.1

---
+++ cdb/byte_copy.c
@@ -0,0 +1,11 @@
+#include "byte.h"
+
+void byte_copy(char *to,unsigned int n,const char *from)
+{
+  for (;;) {
+    if (!n) return; *to++ = *from++; --n;
+    if (!n) return; *to++ = *from++; --n;
+    if (!n) return; *to++ = *from++; --n;
+    if (!n) return; *to++ = *from++; --n;
+  }
+}
